57059a5161842a326dd3072fc4cb7926268b6a6c
hogehoge315 <yojiro0714@gmail.com>
2026-01-18 16:00:32 +0900

    CORSミドルウェアを追加し、フロントエンドのオリジンを許可

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.models.todo import Base
 from app.routers.todos import router as todo_router
 
 app = FastAPI()
+
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # フロントエンドのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # 全てのHTTPメソッドを許可
+    allow_headers=["*"],  # 全てのヘッダーを許可
+)
 
 app.include_router(todo_router)
 
